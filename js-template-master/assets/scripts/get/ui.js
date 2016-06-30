68c9ff0da6188c77725b6e00a5d4724b577a6fb4
erinsul <erin_sullivan50@yahoo.com>
2016-06-30 11:46:55 -0400

    Fix capitalization error
    Fixed capitalization error

--- js-template-master/assets/scripts/get/ui.js
+++ js-template-master/assets/scripts/get/ui.js
@@ -4,7 +4,6 @@ const app = require('../app.js');
 
 const successGetBook = (data) => {
   let bookListing = require('../templates/all-books.handlebars');
-  return booklisting
   $('#content').append(bookListing(data));
 };
 
